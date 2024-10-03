2a6c716a57385b8b529d778bcf4108565408e2f8
Gp32Prog66 <szojo7@hotmail.com>
2024-10-03 12:45:02 -0400

    Added ReadMeFile and WebScraping Cabilities Working

---
+++ README.md
@@ -0,0 +1,9 @@
+# Things Needed to install
+
+BeautifulSoup Library
+
+'''python -m pip install beautifulsoup4
+'''pip install requests
+
+## Run Program
+py WebScraping.py
--- WebScraping.py
+++ WebScraping.py
@@ -1,9 +1,22 @@
 #Will need to grab the request library
+from bs4 import BeautifulSoup
 import requests
 
 #GET Request
-req = requests.get('https://www.pointpark.edu/index.html')
+req = requests.get('http://quotes.toscrape.com')
+soup = BeautifulSoup(req.text, "html.parser")
+
+
+quote = soup.findAll("span", attrs={"class":"text"})
+author = soup.findAll("small", attrs={"class":"author"})
+
+#For Loop
+for quote in quote:
+    print(quote.text)
+for author in author:
+    print(author.text)
 
 #Results
 print(req)
 
+print(req.content)
